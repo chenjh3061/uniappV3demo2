bd108af9ef25f1fa7922ae62644e0c2414c5abbc
cjh <chenwx58668@163.com>
2024-08-06 15:48:51 +0800

    初始化

---
+++ pages/demo1/demo1.vue
@@ -0,0 +1,13 @@
+<template>
+	<view>这是demo1页面</view>
+	<navigator animation-type="pop-out" animation-duration="300" open-type="navigateBack">
+		<button>navigator:back</button>
+	</navigator>
+	<view class="scroll">
+		
+	</view>
+</template>
+
+<script setup></script>
+
+<style></style>
