05dda0e64d21d162312d5861e2170fdd25dfa5e0
cjh <chenwx58668@163.com>
2024-08-07 17:28:48 +0800

    更新demo1

--- pages/demo1/demo1.vue
+++ pages/demo1/demo1.vue
@@ -3,11 +3,47 @@
 	<navigator animation-type="pop-out" animation-duration="300" open-type="navigateBack">
 		<button>navigator:back</button>
 	</navigator>
-	<view class="scroll">
-		
+	<swiper indicator-dots="true" 
+			indicator-active-color="#007aff"
+			indicator-color="white"
+			circular="true"
+			autoplay="true" interval="2000">
+		<swiper-item>
+			<image src="../../static/logo.png"
+					class="pic1"
+					mode="widthFix"></image>
+		</swiper-item>
+		<swiper-item>2</swiper-item>
+		<swiper-item>3</swiper-item>
+	</swiper>
+	<view id="time">
+		{{ localDate }}
 	</view>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+	
+const localDate = ref(new Date().toLocaleString());
+console.log(localDate) 
+</script>
 
-<style></style>
+<style scoped>
+swiper{
+	width: 100vw;
+	height: 150px;
+	border: 1px solid red;
+}
+swiper-item{
+	width: 100%;
+	height: 100%;
+	background: pink;
+}
+.pic1{
+	width: 150px;
+	height: 150px;
+	position: fixed;
+	top: 0;
+	left: 20vw;
+}
+</style>
