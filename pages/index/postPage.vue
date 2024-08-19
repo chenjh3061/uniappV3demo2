d506060b0fb1490d5c831a1f3b2167507939e2aa
俊宏 <oncwnuLAeJWzo2Rk1De_oNwKmI9s@git.weixin.qq.com>
2024-08-19 22:22:12 +0800

    修改用户页面

--- pages/index/postPage.vue
+++ pages/index/postPage.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
 		<view>往期文章</view>
+		<view class="search">
+			<input type="text" :value="searchText.value" 
+					@focus="isActive = true" @blur="isActive=false" 
+					v-model="searchText" @confirm="doSearch"/>
+			<image class="pic" src="../../static/logo.png" 
+					:class="isActive ? 'active':''"></image>
+			<view>预览：{{ searchText }}</view>
+		</view>
 		<view class="post-list" v-for="(post, index) in postList" :key="post.id">
 			<view>{{ post.id }}</view>
 			<view>{{ post.text }}</view>
@@ -9,19 +17,73 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+	import {
+		ref, watchEffect
+	} from 'vue';
+ 
+	const postList = ref([{
+			id: 1,
+			text: "123123123"
+		},
+		{
+			id: 2,
+			text: "123123123"
+		},
+		{
+			id: 3,
+			text: "123123123"
+		},
+		{
+			id: 4,
+			text: "123123123"
+		},
+	])
 
-const postList = ref([
-	{id:1, text:"123123123"},
-	{id:2, text:"123123123"},
-	{id:3, text:"123123123"},
-	{id:4, text:"123123123"},
-])
-console.log(postList.value);
+	const searchText = ref("")
+
+	const isActive = ref(false);
+	
+	const doSearch = (e) => {
+		console.log(e)
+	};
+	
+	watchEffect(() => {
+		searchText.value
+	});
+	
 </script>
 
-<style scoped>
-.post-list {
-	border: 1rpx;
-}
+<style lang="scss" scoped>
+	.post-list {
+		border: 1rpx;
+	}
+
+	.search {
+		padding: 0 20px;
+		margin-top: 40px;
+		margin-left: 2vw;
+		position: relative;
+
+		input {
+			border: 1px solid #00ff7f;
+			height: 40px;
+			width: 80vw;
+			position: relative;
+			z-index: 2;
+			background: #fff;
+		}
+
+		.pic {
+			width: 24px;
+			height: 24px;
+			z-index: 1;
+			position: absolute;
+			left: calc(50% - 12px);
+			transition: top 0.3s;
+		}
+		.pic.active{
+			top: -24;
+		}
+
+	}
 </style>
